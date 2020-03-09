f8cff65bb0b4fe95ecb0f8d2cc2a02f5f63fa03e
heidicrq <chanheidi1999@gmail.com>
2020-03-09 21:05:50 +0800

    allow the window to maximise upon launching the app

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -167,6 +167,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        primaryStage.setMaximized(true);
         logger.info("Starting AddressBook " + MainApp.VERSION);
         ui.start(primaryStage);
     }
