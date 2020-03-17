702cfc017b136b5062f9ed4827ddc18d475c2f6a
NNpanpan <dpnhat.nguyen@gmail.com>
2020-03-17 17:06:05 +0800

    Common interface for stuff on calendar

--- src/main/java/seedu/address/model/util/DailySchedule.java
+++ src/main/java/seedu/address/model/util/DailySchedule.java
@@ -1,4 +1,13 @@
 package seedu.address.model.util;
 
-public interface Schedulable {
+import java.time.LocalTime;
+
+/**
+ * Interface for objects that can be scheduled and shown on calendar.
+ * Objects of this type should return information to compare within the same date,
+ * i.e. beginning time and ending time.
+ */
+public interface DailySchedule {
+    LocalTime getStartTime();
+    LocalTime getEndTime();
 }
