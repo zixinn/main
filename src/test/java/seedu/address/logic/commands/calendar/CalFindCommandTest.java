b5feaf24da6596b2f89ad42fd962d27aba943584
alushingg <ang.lu.shing99@gmail.com>
2020-03-28 01:15:59 +0800

    test for cal command and parser

--- src/test/java/seedu/address/logic/commands/calendar/CalFindCommandTest.java
+++ src/test/java/seedu/address/logic/commands/calendar/CalFindCommandTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands.calendar;
 
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,14 +12,36 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
 public class CalFindCommandTest {
-    private Model model = new ModelManager();
-    private Model expectedModel = new ModelManager();
+
+    private String getDay(int dayIndex) {
+        switch (dayIndex) {
+        case 1:
+            return "MONDAY";
+        case 2:
+            return "TUESDAY";
+        case 3:
+            return "WEDNESDAY";
+        case 4:
+            return "THURSDAY";
+        case 5:
+            return "FRIDAY";
+        case 6:
+            return "SATURDAY";
+        default:
+            return "SUNDAY";
+        }
+    }
 
     @Test
     public void execute_calFind_success() throws CommandException {
         Model tempModel = new ModelManager();
         CommandResult expectedCommandResult = new CalFindCommand().execute(tempModel);
-        assertCommandSuccess(new CalFindCommand(), model, expectedCommandResult, expectedModel);
+        StringBuilder expectedResult = new StringBuilder(CalFindCommand.MESSAGE_SUCCESS);
+        int currentDayOfWeek = LocalDate.now().getDayOfWeek().getValue();
+        for (int i = currentDayOfWeek; i <= 7; i++) {
+            expectedResult.append(getDay(i)).append(":\n00:00-23:59\n");
+        }
+        assertEquals(expectedCommandResult.getFeedbackToUser(), expectedResult.toString());
     }
 
 }
--- src/test/java/seedu/address/logic/parser/calendar/CalViewCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/calendar/CalViewCommandParserTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.parser.calendar;
 
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,29 @@ public class CalViewCommandParserTest {
     private CalViewCommandParser parser = new CalViewCommandParser();
 
     @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CalViewCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "       ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, CalViewCommand.MESSAGE_USAGE));
+    }
+
+    @Test
     public void parse_validField_success() {
         assertParseSuccess(parser, " /week this", new CalViewCommand(Calendar.getNowCalendar()));
         assertParseSuccess(parser, " /week next", new CalViewCommand(Calendar.getNextWeekCalendar()));
+    }
+
+    @Test
+    public void parse_invalidField_throwsParseException() {
+        //empty input
+        assertParseFailure(parser, " /week",
+                Calendar.MESSAGE_CONSTRAINTS);
+
+        //invalid word
+        assertParseFailure(parser, " /week those",
+                Calendar.MESSAGE_CONSTRAINTS);
+
     }
 
 }
