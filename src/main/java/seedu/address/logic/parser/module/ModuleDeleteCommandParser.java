dd9267f61d0070ce9649b75241367ceff2a1430d
alushingg <50512903+alushingg@users.noreply.github.com>
2020-03-30 20:08:31 +0800

    Update ModuleDeleteCommandParser.java

--- src/main/java/seedu/address/logic/parser/module/ModuleDeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/module/ModuleDeleteCommandParser.java
@@ -13,7 +13,6 @@ import seedu.address.model.module.ModuleCode;
  * Parses input arguments and creates a new ModuleDeleteCommand object.
  */
 public class ModuleDeleteCommandParser implements Parser<ModuleDeleteCommand> {
-    
     /**
      * Parses the given {@code String} of arguments in the context of the ModuleDeleteCommand
      * and returns a ModuleDeleteCommand object for execution.
