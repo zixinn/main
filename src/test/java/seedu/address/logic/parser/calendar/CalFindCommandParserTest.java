eff7d4a12c70c074a04e5e58df201c24df78e0e8
alushingg <ang.lu.shing99@gmail.com>
2020-04-06 00:04:59 +0800

    check for too many arguments for calendar

--- src/test/java/seedu/address/logic/parser/calendar/CalFindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/calendar/CalFindCommandParserTest.java
@@ -1,6 +1,7 @@
 package seedu.address.logic.parser.calendar;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.EMPTY_SPACES;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -16,7 +17,7 @@ public class CalFindCommandParserTest {
     public void parse_emptyArg_throwsParseException() {
         assertParseFailure(parser, "",
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, CalFindCommand.MESSAGE_USAGE));
-        assertParseFailure(parser, "       ",
+        assertParseFailure(parser, EMPTY_SPACES,
                 String.format(MESSAGE_INVALID_COMMAND_FORMAT, CalFindCommand.MESSAGE_USAGE));
     }
 
