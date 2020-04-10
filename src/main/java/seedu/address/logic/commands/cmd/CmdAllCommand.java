76d53cd58dd5ba77b6efcb71e5a6fd9517201a65
NNpanpan <dpnhat.nguyen@gmail.com>
2020-04-10 20:02:52 +0800

    Updates

--- src/main/java/seedu/address/logic/commands/cmd/CmdAllCommand.java
+++ src/main/java/seedu/address/logic/commands/cmd/CmdAllCommand.java
@@ -14,16 +14,25 @@ public class CmdAllCommand extends CmdCommand {
     public static final String MESSAGE_USAGE = COMMAND_GROUP_CMD + " " + COMMAND_WORD_ALL
             + ": Lists all the command groups available in the system.";
 
-    private final String instruction = "Use " + COMMAND_GROUP_CMD + " " + COMMAND_WORD_GROUP
+    private final String instruction = "Type: " + COMMAND_GROUP_CMD + " " + COMMAND_WORD_GROUP
             + " COMMAND_GROUP to find out about a particular command group.";
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
         StringBuilder builder = new StringBuilder();
         builder.append("Here are all available command groups: \n");
-        Command.ALL_COMMAND_GROUPS.forEach(str -> builder.append(str).append(" "));
-        builder.append("\n").append(instruction);
 
+        boolean first = true;
+        for (String group : Command.ALL_COMMAND_GROUPS) {
+            if (first) {
+                builder.append(group);
+                first = false;
+            } else {
+                builder.append(", ").append(group);
+            }
+        }
+
+        builder.append("\n").append(instruction);
         return new CommandResult(builder.toString(), CommandType.CMD);
     }
 }
